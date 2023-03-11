5880c96fd44114e2fd9455ed7f4adcc5e307e309
StillFantasyC <111132969+StillFantasyC@users.noreply.github.com>
2023-03-11 22:12:55 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -21,11 +21,11 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '雪宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ofA0F5ywQOnbr_zMsm5Nx9UGLEfg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'TZ99HKodSyTn7Wpv5xyQg1oijlV0sONpBbtiwT-FFEc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '04-26',
+      horoscopeDate: '04-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -33,10 +33,10 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '宝贝', year: '2004', date: '04-26',
+          type: '生日', name: '宝贝', year: '2004', date: '04-27',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '10-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -44,7 +44,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2022-10-03' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2023-09-09' },
+        { keyword: 'marry_day', date: '2027-10-03' },
       ],
     },
   ],
